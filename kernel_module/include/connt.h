1f00e3bdf7819a8d7499c5b86ee20861dac7f5c6
SimoneDutto <simonedutto8@gmail.com>
2020-06-22 18:00:46 +0200

    Add simple comunication btw process and kernel module

---
+++ kernel_module/include/connt.h
@@ -0,0 +1,7 @@
+#ifndef _CONNT_H
+#define _CONNT_H
+
+int tcp_client_connect(void);
+int tcp_sendf_waitr(int *features);
+
+#endif
--- kernel_module/src/monitor.c
+++ kernel_module/src/monitor.c
@@ -4,12 +4,20 @@
 #include <linux/pid.h>
 #include <linux/sched.h>
 #include <linux/sched/task.h>
+#include <linux/rcupdate.h>
+
+#include "../include/connt.h"
+
+MODULE_AUTHOR("Simone Dutto");
+MODULE_DESCRIPTION("module to evaluate faultry proc");
+MODULE_LICENSE("GPL");
 
 static void mykmod_work_handler(struct work_struct *w);
 
 static struct workqueue_struct *wq = 0;
 static DECLARE_DELAYED_WORK(mykmod_work, mykmod_work_handler);
 static unsigned long onesec;
+static unsigned int *features = {0,0};
 
 static void
 mykmod_work_handler(struct work_struct *w)
@@ -17,19 +25,21 @@ mykmod_work_handler(struct work_struct *w)
 	struct task_struct *tsk;
         pr_info("mykmod work %u jiffies\n", (unsigned)onesec);
 	
-	read_lock(&tasklist_lock);
-	tsk = pid_task(find_vpid(12345), PIDTYPE_PID);
+	rcu_read_lock();
+	tsk = pid_task(find_vpid(2031), PIDTYPE_PID);
 	if(tsk == NULL)
 		pr_info("No process with this PID");
-	pr_info("Ciao");
-	read_unlock(&tasklist_lock);
+	pr_info("Found the process");
+	tcp_sendf_waitr(features);
+	
+	rcu_read_unlock();
 }
 
 int init_module(void)
 {
         onesec = msecs_to_jiffies(1000);
         pr_info("mykmod loaded %u jiffies\n", (unsigned)onesec);
-
+	tcp_client_connect();
         if (!wq)
                 wq = create_singlethread_workqueue("mykmod");
         if (wq)
@@ -49,5 +59,3 @@ void cleanup_module(void)
 }
 
 
-MODULE_DESCRIPTION("mykmod");
-MODULE_LICENSE("GPL");
